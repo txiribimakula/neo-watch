45f09c93125a60e916d175b6cbf41e7a09bc7229
Pablo Tobalina <pablotobalina@gmail.com>
2023-11-12 20:30:21 +0100

    add feedback class

---
+++ NeoWatch.Common/Feedback.cs
@@ -0,0 +1,22 @@
+﻿namespace NeoWatch.Common
+{
+    public class Feedback
+    {
+        private bool hasError = false;
+
+        public Feedback()
+        {
+            Description = "OK";
+        }
+
+        public Feedback(string description)
+        {
+            Description = description;
+            hasError = true;
+        }
+
+        public bool HasError => hasError;
+
+        public string Description { get; private set; }
+    }
+}
