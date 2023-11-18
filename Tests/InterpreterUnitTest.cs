308583e66b0f8a5acb533c5e31b1638ecf912a15
Pablo Tobalina <pablotobalina@gmail.com>
2023-11-18 10:18:08 +0100

    unit test getting drawable segment

--- Tests/InterpreterUnitTest.cs
+++ Tests/InterpreterUnitTest.cs
@@ -1,3 +1,5 @@
+using NeoWatch.Drawing;
+using NeoWatch.Geometries;
 using NeoWatch.Loading;
 using Tests.Mocks;
 
@@ -101,6 +103,54 @@ namespace Tests
             {
                 // Arrange
                 var expressionMock = new ExpressionMock(value, parse);
+
+                // Act
+                var drawable = interpreter.GetDrawable(expressionMock);
+
+                // Assert
+                Assert.Null(drawable);
+            }
+        }
+
+        public class Get_Drawable_Segment
+        {
+            Interpreter interpreter;
+
+            public Get_Drawable_Segment()
+            {
+                interpreter = new Interpreter(patterns, typeKindPairs);
+            }
+
+            [Fact]
+            public void returns_expected_segment()
+            {
+                // Arrange
+                var expressionMock = new ExpressionMock("Seg: Pnt: (5.00,15.00) - Pnt: (100.00,10.00)");
+
+                // Act
+                var drawable = interpreter.GetDrawable(expressionMock);
+
+                // Assert
+                Assert.NotNull(drawable);
+                Assert.True(drawable.Box.IsValid);
+                var expectedDrawableLineSegment = new DrawableLineSegment(new Point(5, 15), new Point(100, 10));
+                Assert.Equivalent(expectedDrawableLineSegment, drawable);
+            }
+
+
+            [Theory]
+            [InlineData("Seg: Pnt: (,) - Pnt: (,)")]
+            [InlineData("Seg: Pnt: (5,00,15.00) - Pnt: (100.00,10.00)")]
+            [InlineData("Seg: Pnt: (5.00,15,00) - Pnt: (100.00,10.00)")]
+            [InlineData("Seg: Pnt: ((5.00,15.00) - Pnt: (100.00,10.00)")]
+            [InlineData("Segment: Point: (5.00,15.00) - Point: (100.00,10.00)")]
+            [InlineData("Seg: Pnt: 5.00,15.00) - Pnt: (100.00,10.00)")]
+            [InlineData("Seg: Pnt: (5.00,15.00) Pnt: (100.00,10.00)")]
+            [InlineData("Pnt: (5.00,15.00) - Pnt: (100.00,10.00)")]
+            public void returns_invalid_segment(string value)
+            {
+                // Arrange
+                var expressionMock = new ExpressionMock(value);
 
                 // Act
                 var drawable = interpreter.GetDrawable(expressionMock);
