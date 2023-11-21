4d196bbbb4573709d27203408c025149d908ad9f
Pablo Tobalina <pablotobalina@gmail.com>
2023-11-21 20:08:23 +0100

    unit test loader

---
+++ Tests/LoaderUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using NeoWatch.Loading;
+using Tests.Mocks;
+
+namespace Tests
+{
+    public class LoaderUnitTest
+    {
+        public class Load_Item
+        {
+            private Loader loader;
+
+            public Load_Item()
+            {
+                loader = new Loader(new DebuggerMock(), new InterpreterMock());
+            }
+
+            [Fact]
+            public async void returns_drawables()
+            {
+                // Arrange
+
+
+                // Act
+                await Lo
+
+                // Assert
+            }
+
+        }
+    }
+}
